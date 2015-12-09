306e30572983dde65246dcf1b9bb7fd8389dade9
Stefan Kjartansson <esteban.supreme@gmail.com>
2015-12-09 17:26:32 +0000

    Has bug, needs fix

--- frontend/song/index.js
+++ frontend/song/index.js
@@ -1,15 +1,49 @@
 import React from 'react';
+import Store from '../Store.js';
+import FlatButton from 'material-ui';
 
 export default class Song extends React.Component {
 
+  constructor(props, context) {
+    super(props, context);
+    this.state = {dialog: false, loading: true};
+  }
+
   componentDidMount() {
-    console.log(this.props.params.songId);
-    // check access. if no access, show dialog.
+    let d = Store.getById(this.props.params.songId);
+    d.loading = false;
+    this.setState(d);
+  }
+
+  get player() {
+    if (!this.state.access) {
+      return <div/>;
+    }
+    return (
+      <span>{this.state.url}</span>
+    );
+  }
+
+  get dialogLauncher() {
+   if (this.state.access) {
+      return <div/>;
+    }
+    return (
+      <FlatButton label="Buy" primary={true} onClick={() => setState({dialog: true})} />
+    );
   }
 
   render() {
+    if (this.state.loading) {
+      return <div />;
+    }
+    console.log(this.state);
     return (
-      <div>foo</div>
+      <div>
+        <h1>{this.state.title}</h1>
+        {this.player}
+        {this.dialogLauncher}
+      </div>
     );
   }
 
