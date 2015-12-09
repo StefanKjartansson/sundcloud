bcfc96a349c12b0d9e16accec2fcf32cbc8ea998
Stefan Kjartansson <esteban.supreme@gmail.com>
2015-12-09 16:48:23 +0000

    Add init, use httprouter

--- main.go
+++ main.go
@@ -9,8 +9,40 @@ import (
 	"os"
 
 	"github.com/StefanKjartansson/sundcloud/laterpay"
+	"github.com/julienschmidt/httprouter"
 	"github.com/satori/go.uuid"
 )
+
+var c laterpay.LaterPayClient
+var catalog []Song
+
+func init() {
+	merchantID := os.Getenv("LP_ID")
+	merchantSecret := os.Getenv("LP_SECRET")
+
+	if merchantID == "" {
+		log.Fatalln("LP_ID must be set")
+	}
+
+	if merchantSecret == "" {
+		log.Fatalln("LP_SECRET must be set")
+	}
+
+	c = laterpay.LaterPayClient{
+		Id:        merchantID,
+		SecretKey: []byte(merchantSecret),
+		AddURL:    "/foo",
+		AccessURL: "/bar",
+		WebRoot:   "/baz",
+	}
+	catalog = []Song{
+		{uuid.NewV4().String(), "Adele", "Hello", "http://lorempixel.com/200/100/", "/mp3/adele.mp3", true},
+		{uuid.NewV4().String(), "Foo", "World", "http://lorempixel.com/200/100/", "/mp3/adele.mp3", true},
+		{uuid.NewV4().String(), "Rammstein", "Bobo", "http://lorempixel.com/200/100/", "/mp3/adele.mp3", true},
+		{uuid.NewV4().String(), "Fungi", "XXXX", "http://lorempixel.com/200/100/", "/mp3/adele.mp3", true},
+	}
+
+}
 
 type Song struct {
 	Id     string `json:"id"`
@@ -20,8 +52,6 @@ type Song struct {
 	Url    string `json:"url"`
 	Access bool   `json:"access"`
 }
-
-const getToken = "https://api.sandbox.laterpaytest.net/gettoken"
 
 const tpl = `
 <!doctype html>
@@ -62,96 +92,71 @@ func getIdsFromCatalog(songs []Song) []string {
 	return out
 }
 
-func main() {
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	Token := r.URL.Query().Get("lptoken")
 
-	merchantID := os.Getenv("LP_ID")
-	merchantSecret := os.Getenv("LP_SECRET")
-
-	if merchantID == "" {
-		log.Fatalln("LP_ID must be set")
+	if Token == "" {
+		u, err := c.GetTokenURL("http://46.101.229.91/")
+		if err != nil {
+			http.Error(w, fmt.Sprintf("%s", err), 400)
+			return
+		}
+		http.Redirect(w, r, u, 302)
+		return
 	}
 
-	if merchantSecret == "" {
-		log.Fatalln("LP_SECRET must be set")
+	data := struct {
+		Token string
+	}{
+		Token,
 	}
-
 	t, err := template.New("webpage").Parse(tpl)
 	if err != nil {
 		log.Fatalln("Unable to parse index")
 	}
 
-	catalog := []Song{
-		{uuid.NewV4().String(), "Adele", "Hello", "http://lorempixel.com/200/100/", "/mp3/adele.mp3", true},
-		{uuid.NewV4().String(), "Foo", "World", "http://lorempixel.com/200/100/", "/mp3/adele.mp3", true},
-		{uuid.NewV4().String(), "Rammstein", "Bobo", "http://lorempixel.com/200/100/", "/mp3/adele.mp3", true},
-		{uuid.NewV4().String(), "Fungi", "XXXX", "http://lorempixel.com/200/100/", "/mp3/adele.mp3", true},
-	}
+	err = t.Execute(w, data)
 
-	c := laterpay.LaterPayClient{
-		Id:        merchantID,
-		SecretKey: []byte(merchantSecret),
-		AddURL:    "/foo",
-		AccessURL: "/bar",
-		WebRoot:   "/baz",
-	}
+}
 
-	http.HandleFunc("/api/songs/", func(w http.ResponseWriter, r *http.Request) {
-		enc := json.NewEncoder(w)
-		localCatalog := catalog[:]
-		token := r.Header.Get("X-LP-Token")
+func API(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	enc := json.NewEncoder(w)
+	localCatalog := catalog[:]
+	token := r.Header.Get("X-LP-Token")
 
-		ids := getIdsFromCatalog(localCatalog)
+	ids := getIdsFromCatalog(localCatalog)
 
-		accessStats := c.Access(token, ids...)
+	accessStats := c.Access(token, ids...)
 
-		for id, access := range accessStats {
-			if access {
-				continue
-			}
-			for idx, l := range localCatalog {
-				if l.Id == id {
-					i := laterpay.ItemDefinition{
-						Id:      id,
-						Pricing: "EUR23",
-						Title:   l.Title,
-					}
-					url, err := c.Add(i)
-					if err != nil {
-						log.Println(err)
-					}
-					localCatalog[idx].Url = url
-					localCatalog[idx].Access = false
+	for id, access := range accessStats {
+		if access {
+			continue
+		}
+		for idx, l := range localCatalog {
+			if l.Id == id {
+				i := laterpay.ItemDefinition{
+					Id:      id,
+					Pricing: "EUR23",
+					Title:   l.Title,
 				}
+				url, err := c.Add(i)
+				if err != nil {
+					log.Println(err)
+				}
+				localCatalog[idx].Url = url
+				localCatalog[idx].Access = false
 			}
 		}
+	}
 
-		enc.Encode(localCatalog)
-	})
+	enc.Encode(localCatalog)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// IF no lptoken in q, redirect.
-		Token := r.URL.Query().Get("lptoken")
+}
 
-		if Token == "" {
-			u, err := c.GetTokenURL("http://46.101.229.91/")
-			if err != nil {
-				http.Error(w, fmt.Sprintf("%s", err), 400)
-				return
-			}
-
-			http.Redirect(w, r, u, 302)
-			return
-		}
-
-		data := struct {
-			Token string
-		}{
-			"Token",
-		}
-		err = t.Execute(w, data)
-	})
-
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", fs)
-	log.Fatal(http.ListenAndServe(":80", nil))
+func main() {
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/api/songs/", API)
+	router.ServeFiles("/static/*filepath", http.Dir("static"))
+	log.Fatal(http.ListenAndServe(":80", router))
 }
